2588c690161a0b71553831bef6a40824d0029cee
HarrisK <HarrisK@github.com>
2021-05-27 18:36:56 +0500

    Added PWA

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const path = require('path');
+const enforce = require('express-sslify');
 
+const app = express();
+const port = process.env.PORT || 3000;
 
-const app = express()
-const port = process.env.PORT || 3000
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
-app.listen(port, () => console.log("app is running"))
+app.listen(port, () => console.log('app is running'));
+
+app.get('/service-worker.js', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'src', 'service-worker.js'));
+});
